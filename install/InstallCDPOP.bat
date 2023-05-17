d0e79fcc5da152a787e3089f3d1421d2b3cd033a
Ivan Gonzalez <gonzalezgarzonivan@gmail.com>
2023-05-16 20:00:11 -0500

    add pause

--- install/InstallCDPOP.bat
+++ install/InstallCDPOP.bat
@@ -7,10 +7,20 @@ cd %USERPROFILE%
 md dockerdata
 cd dockerdata
 curl https://raw.githubusercontent.com/gonzalezivan90/connectscape/main/install/dockerfile -O dockerfile
+curl https://raw.githubusercontent.com/rbachati/cdpop_mui/main/test.py -O test.py
+
+
+
 :: Be sure docker is running with the login
+:: "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+docker stop cdpop
+docker rm cdpop
+docker rmi cdpop_image
 docker build -t cdpop_image . 
-# docker run -i -t -p 8888:8888 --name cdpop -v %USERPROFILE%\dockerdata:/dockerdata cdpop_image 
+:: docker run -i -t -p 8888:8888 --name cdpop -v %USERPROFILE%\dockerdata:/dockerdata cdpop_image 
 docker run -d -i -t -p 8501:8501 --name cdpop -v %USERPROFILE%\dockerdata:/dockerdata cdpop_image 
 
 :: docker exec -it cdpop /bin/bash # Get into the container
+::explorer "http://localhost:8501/"
+rundll32 url.dll,FileProtocolHandler http://localhost:8501/
 pause
